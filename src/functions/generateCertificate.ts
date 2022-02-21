3ba934b7e8e81131de99c86ce4e16554f72d7a78
henryque <henryque_1@hotmail.com>
2022-02-21 11:10:20 -0400

    refactor: readjust to create bucket and send pdf to aws s3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -9,6 +9,8 @@ import { join } from "path";
 import { readFileSync } from "fs";
 import chromium from "chrome-aws-lambda";
 
+import { S3 } from "aws-sdk";
+
 interface ICreateCertificate {
   id: string;
   name: string;
@@ -34,18 +36,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document
-    .put({
-      TableName: "users_certificate",
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      },
-    })
-    .promise();
-
   const response = await document
     .query({
       TableName: "users_certificate",
@@ -55,6 +45,22 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const userAlreadyExists = response.Items[0];
+
+  if (!userAlreadyExists) {
+    await document
+      .put({
+        TableName: "users_certificate",
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
 
   const medalPath = join(process.cwd(), "src", "templates", "selo.png");
   const medal = readFileSync(medalPath, "base64");
@@ -88,8 +94,29 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   await browser.close();
 
+  const s3 = new S3();
+
+  // await s3
+  //   .createBucket({
+  //     Bucket: "certificateigniterodz2022",
+  //   })
+  //   .promise();
+
+  await s3
+    .putObject({
+      Bucket: "certificateigniterodz2022",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf",
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0]),
+    body: JSON.stringify({
+      message: "Certificate created successfully!",
+      url: `https://certificateigniterodz2022.s3.sa-east-1.amazonaws.com/${id}.pdf`,
+    }),
   };
 };
